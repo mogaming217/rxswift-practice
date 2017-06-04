baeb437c786e02e13285130eb9d08a605003fa64
最上聖也 <seiya.orz@gmail.com>
2017-06-04 23:36:24 +0900

    textFieldとボタンで遊んでみた

--- rxswift-practice/ViewController.swift
+++ rxswift-practice/ViewController.swift
@@ -7,17 +7,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var secondField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        // orEmptyで空文字やnilはObserve対象外
+        let firstTextFieldObservable = firstTextField.rx.text.orEmpty.asObservable()
+        
+        firstTextFieldObservable
+            // 指定した秒数以下のものはObserveしないらしい
+            .throttle(2, scheduler: MainScheduler.instance)
+            .subscribe(onNext: { text in
+                if text.characters.count >= 5 {
+                    print(text)
+                }
+            })
+            .addDisposableTo(disposeBag)
+        
+        let secondFieldObservable = secondField.rx.text.orEmpty.asObservable()
+        secondFieldObservable
+            .bindTo(secondLabel.rx.text)
+            .addDisposableTo(disposeBag)
+        
+        // 2つのテキストフィールドに何かしら入力されていたらボタン有効化
+        Observable.combineLatest(firstTextFieldObservable, secondFieldObservable) { str1, str2 in
+            return str1.characters.count > 0 && str2.characters.count > 0
+        }.bindTo(button.rx.isEnabled)
+        .addDisposableTo(disposeBag)
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
