1388a2c8c14267438e195585f9b722557fb0bfe1
最上聖也 <seiya.orz@gmail.com>
2017-06-04 22:22:20 +0900

    initial commit

---
+++ rxswift-practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rxswift-practice
+//
+//  Created by 最上聖也 on 2017/06/04.
+//  Copyright © 2017年 SeiyaMogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
